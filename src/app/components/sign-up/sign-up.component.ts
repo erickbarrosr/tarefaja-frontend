ba5b9a4419907cd34d000544317b00862a06c209
Erick Barros <erick@sensio.com.br>
2025-01-03 14:16:24 -0300

    fix

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -4,7 +4,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
   signUpForm!: FormGroup;
