4b0032f382d7479fd22d5affb600938881b08506
Erick Barros <erick@sensio.com.br>
2025-01-03 14:15:02 -0300

    feat(sign-up.component.ts): created sign-up component class

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+})
+export class SignUpComponent {
+  signUpForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.buildForms();
+  }
+
+  buildForms() {
+    this.signUpForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+    });
+  }
+
+  onFormSubmit() {
+    if (this.signUpForm.valid) {
+      const formValue = this.signUpForm.value;
+
+      console.log(formValue);
+    }
+  }
+}
